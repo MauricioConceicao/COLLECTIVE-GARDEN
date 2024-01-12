98350e61c896a6f32879cbe571580795776d6b8c
Mauricio Conceição <58689369+MauricioConceicao@users.noreply.github.com>
2024-01-11 22:30:35 -0300

    Atualização 0.1 11/01/2024
    feita algumas atualizaçoes adicionais no splashscreen

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // ReferÃªncia Ã  janela existente
+    // Referencia a janela existente
     var janelaExistente = document.getElementById('janelaExistente');
 
     // Estilizando a janela existente
     janelaExistente.style.position = 'fixed';
     janelaExistente.style.top = '50%';
     janelaExistente.style.left = '50%';
+    janelaExistente.style.zIndex = 1;
     janelaExistente.style.transform = 'translate(-50%, -50%)';
 
     // Exibindo a janela existente
     janelaExistente.style.display = 'block';
 
-    // Definindo um timeout para ocultar a janela apÃ³s alguns milissegundos
+    // Definindo um timeout para ocultar a janela por alguns milissegundos
     setTimeout(function() {
         janelaExistente.style.display = 'none';
-    }, 4000); // Substitua 2000 pelo nÃºmero desejado de milissegundos
+    }, 4000); //tempo desejado de milissegundos
     });
 
 function minhaFuncao() {
