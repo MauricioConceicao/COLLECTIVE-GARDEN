93de4174df94eae880356019823cf2a996feca67
Mauricio Conceição <58689369+MauricioConceicao@users.noreply.github.com>
2024-06-27 01:28:37 -0300

    banco

--- collective_garden.sql
+++ collective_garden.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Tempo de geração: 06-Abr-2024 às 05:39
+-- Tempo de geração: 26-Jun-2024 às 13:59
 -- Versão do servidor: 10.4.28-MariaDB
 -- versão do PHP: 8.1.17
 
@@ -20,6 +20,27 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `collective_garden`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cadastro_plantas`
+--
+
+CREATE TABLE `cadastro_plantas` (
+  `latitude` text NOT NULL,
+  `longetude` text NOT NULL,
+  `nome_comum` varchar(25) NOT NULL,
+  `nome_cientifico` varchar(25) NOT NULL,
+  `observacao` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `cadastro_plantas`
+--
+
+INSERT INTO `cadastro_plantas` (`latitude`, `longetude`, `nome_comum`, `nome_cientifico`, `observacao`) VALUES
+('-30.089569844094086', '-51.3371479511261', 'hortela', '', 'em frente ao apartamento da falecida dna');
 
 -- --------------------------------------------------------
 
@@ -47,7 +68,10 @@ INSERT INTO `cadastro_usuario` (`nome`, `sobrenome`, `email`, `senha`) VALUES
 ('abdiel', 'pumpum', 'pumpum@gmail.com', '12345a'),
 ('carol', 'oliveira', 'carol@gmail.com', '123'),
 ('carol', 'oliveira', 'carol@gmail.com', '123'),
-('carol', 'oliveira', 'carol@gmail.com', '123');
+('carol', 'oliveira', 'carol@gmail.com', '123'),
+('bruna', 'conceicao', 'bruna@gmail.com', '123'),
+('maria ', 'deniseconceiçao', 'conceicaodenisemaria6@gmail.com', '034113560d'),
+('maia', 'conceicao', 'conceicaodenisemaria6@gmail.com', '034113560d');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
